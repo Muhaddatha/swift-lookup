837c5e25e66f2423a507fc58a8faedfd20854690
Muhaddatha Abdulghani <mzabdulg@umich.edu>
2020-12-26 15:28:27 -0500

    request made to api

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,6 @@
 let wordToLookup;
-let enLink = "https://api.dictionaryapi.dev/api/v2/entries/en/";
+let dataFromAPI;
+
 
 $(document).ready(function(){
 
@@ -11,6 +12,8 @@ $(document).ready(function(){
 
 
 function searchWord(e){
+
+    let enLink = "https://api.dictionaryapi.dev/api/v2/entries/en/";
     
     e.preventDefault();
     
@@ -29,6 +32,45 @@ function searchWord(e){
    enLink += wordToLookup;
    console.log("Link to look up: " + enLink);
 
-   
-   
+   connectToAPI(enLink);
+
+}
+
+
+//arrow function to retrieve JSON data
+let connectToAPI = theLink => {
+
+    let success = true;
+
+    console.log("Inside the connect to API function");
+
+    fetch(theLink) //fetch only catches network errors not 404 errors
+    .then(res => {
+        if(res.ok){
+            console.log("Success!");
+            // ok response receivede
+            return res.json() 
+            // turning the response into readable json format
+        }
+        else{
+            console.log("Not successful");
+            success = false;
+            // got a 404 response
+            //inform user word was not correct
+        }
+        
+    })
+    .then(dataFromAPI => {
+        if(success){
+            console.log(dataFromAPI);
+        }
+        else{
+            console.log("error occured! word spelled incorrectly");
+        }
+        
+    })
+
+    console.log("Returning json data from connect to API function")
+    // return data;
+
 }
